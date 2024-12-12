e961c939c35d67dea1a70831a4299539c24a222e
Raphael Marques <102549377+euphael@users.noreply.github.com>
2024-12-12 15:41:50 -0300

    Add files via upload

--- Portfolio Raphael Marques/script.js
+++ Portfolio Raphael Marques/script.js
@@ -1,10 +1,3 @@
 // Seleciona o botão e a área da mensagem
 const button = document.getElementById("clickButton");
 const message = document.getElementById("message");
-
-// Adiciona um evento de clique no botão
-button.addEventListener("click", function() {
-    message.textContent = "Você clicou no botão! 🎉";
-    message.style.color = "#4CAF50"; // Muda a cor da mensagem
-});
-
